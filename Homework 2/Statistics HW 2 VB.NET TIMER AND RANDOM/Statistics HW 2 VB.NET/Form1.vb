512c7fbb79411d3fdc7d810c79900699eb9bc245
Alepica91 <41958449+Alepica91@users.noreply.github.com>
2022-10-12 19:53:24 +0200

    Add files via upload

---
+++ Homework 2/Statistics HW 2 VB.NET TIMER AND RANDOM/Statistics HW 2 VB.NET/Form1.vb
@@ -0,0 +1,34 @@
+﻿
+
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Header
+
+Public Class Form1
+
+    Shared random As New Random()
+    Dim timeleft = 30
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        timer1.Interval = 1000
+        timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Timer1.Stop()
+        timeleft = 30
+        Label1.Text = timeleft.ToString()
+        Label2.Text = ""
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (timeleft > 0) Then
+            timeleft -= 1
+            Label1.Text = timeleft.ToString()
+            Label2.Text = random.Next(100)
+        Else
+            Timer1.Stop()
+        End If
+    End Sub
+End Class
